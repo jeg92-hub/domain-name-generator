ffc413f9c38aa021e36e2b7319ae9608497e8481
joneganagranado <joneganagranado@hotmail.com>
2021-09-16 08:08:50 +0000

    done

---
+++ app.js
@@ -0,0 +1,16 @@
+let pronoun = ['the','our'];
+let adj = ['great', 'big' ];
+let noun = ['jogger','racoon'];
+let extensions = ['com','es','eu','cat']
+
+let res = [];
+console.log(pronoun);
+pronoun.forEach(function(p){
+    adj.forEach(function(a){
+        noun.forEach(function(n){
+            extensions.forEach(function(ext){
+                console.log(p+a+n+"."+ext)
+            })
+        })
+    })
+})
